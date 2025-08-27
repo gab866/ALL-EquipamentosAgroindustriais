3a0fd55cfe2110851097584151ac3534212ea5f4
Gabriel Silveira <gab866@gmail.com>
2025-08-27 15:06:54 -0300

    2025 direitos reservados

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,4 @@
-import './Footer.scss';
+import "./Footer.scss";
 
 const Footer = () => {
   return (
@@ -7,12 +7,17 @@ const Footer = () => {
         <div className="footer-content">
           <div className="footer-info">
             <h3>ALL Equipamentos Agroindustriais</h3>
-            <p>© 2024 Todos os direitos reservados</p>
+            <p>© 2025 Todos os direitos reservados</p>
           </div>
           <div className="footer-contact">
             <div className="phone">
-              <svg width="20" height="20" viewBox="0 0 24 24" fill="currentColor">
-                <path d="M6.62 10.79c1.44 2.83 3.76 5.14 6.59 6.59l2.2-2.2c.27-.27.67-.36 1.02-.24 1.12.37 2.33.57 3.57.57.55 0 1 .45 1 1V20c0 .55-.45 1-1 1-9.39 0-17-7.61-17-17 0-.55.45-1 1-1h3.5c.55 0 1 .45 1 1 0 1.25.2 2.45.57 3.57.11.35.03.74-.25 1.02l-2.2 2.2z"/>
+              <svg
+                width="20"
+                height="20"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+              >
+                <path d="M6.62 10.79c1.44 2.83 3.76 5.14 6.59 6.59l2.2-2.2c.27-.27.67-.36 1.02-.24 1.12.37 2.33.57 3.57.57.55 0 1 .45 1 1V20c0 .55-.45 1-1 1-9.39 0-17-7.61-17-17 0-.55.45-1 1-1h3.5c.55 0 1 .45 1 1 0 1.25.2 2.45.57 3.57.11.35.03.74-.25 1.02l-2.2 2.2z" />
               </svg>
               <span>(53) 99934-6782</span>
             </div>
